586cb540c20ef7e33e84c5ce1e6ee36786e40594
Benlem <zoubidabenlem@gmail.com>
2024-08-13 14:57:22 +0100

    routing exam view

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     # ex: /enroll/5/
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
     path('<int:course_id>/submit/', views.submit, name="submit"),
-    # <HINT> Create a route for show_exam_result view
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name="exam_result"),
+
 
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
